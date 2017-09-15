eb666b11041f1d93d7042055588e52d49af818cd
EDGE <edge@EDGEs-Mac.local>
2017-09-15 16:05:04 -0500

    practica 1 ya esta hecha !!!

--- test2/Home.m
+++ test2/Home.m
@@ -14,10 +14,49 @@
 
 @implementation Home
 
+int state=0;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+- (IBAction)botonsito:(id)sender {
+    
+    NSDate *currentDate = [NSDate date];
+    NSString *textoParaMostrar;
+    
+    if(state==0)  {
+        textoParaMostrar = @"the time is .. tap again";
+    }
+    else if(state==1)  {
+        NSDateFormatter *timeStamp = [[NSDateFormatter alloc] init];
+        [timeStamp setDateFormat:@"HH:mm:ss"];
+        
+        textoParaMostrar = [timeStamp stringFromDate:currentDate];
+    }
+    else if(state==2)  {
+        textoParaMostrar = @"and the date is .. try again";
+    }
+    else if(state==3)  {
+        NSDateFormatter *dayStamp = [[NSDateFormatter alloc] init];
+        [dayStamp setDateFormat:@"dd-MM-yyyy"];
+        
+        textoParaMostrar = [dayStamp stringFromDate:currentDate];
+    }
+    else if(state==4)  {
+        NSDateFormatter *wholeDate = [[NSDateFormatter alloc] init];
+        [wholeDate setDateFormat:@"dd-MM-yyyy HH:mm:ss"];
+        
+        textoParaMostrar =[wholeDate stringFromDate:currentDate];
+        textoParaMostrar =[textoParaMostrar stringByAppendingString:@" es la fecha"];
+        
+        state=-1;
+    }
+    
+    self.lblBotonsito.text =textoParaMostrar;
+    
+    state++;
+ }
 
 
 - (void)didReceiveMemoryWarning {
